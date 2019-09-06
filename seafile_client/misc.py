107bb55082fc737d17dfbf2a5e8bfcbb94568da5
Maks Snegov <snegov@spqr.link>
2019-09-06 21:41:43 +0300

    Move SeafileClient out of main file

---
+++ seafile_client/misc.py
@@ -0,0 +1,25 @@
+import os
+import pwd
+import subprocess
+
+from .client import DEFAULT_USERNAME
+
+
+def setup_uid(uid: int, gid: int):
+    user_pwinfo = pwd.getpwnam(DEFAULT_USERNAME)
+    if user_pwinfo.pw_uid != uid or user_pwinfo.pw_gid != gid:
+        subprocess.call(["usermod", "-o", "-u", str(uid), "-g", str(gid), DEFAULT_USERNAME])
+
+
+def create_dir(dir_path: str):
+    if not os.path.exists(dir_path):
+        os.mkdir(dir_path)
+        user_pwinfo = pwd.getpwnam(DEFAULT_USERNAME)
+        os.chown(dir_path, user_pwinfo.pw_uid, user_pwinfo.pw_gid)
+    else:
+        if not os.path.isdir(dir_path):
+            raise RuntimeError(f"Data dir {dir_path} is not a directory")
+
+
+def tail_f(fpath):
+    os.system(f"tail -f {fpath}")
